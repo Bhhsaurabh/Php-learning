eee484dd6fbba3a3864a51f8de4c0194b64e9207
Saurabh Kumar <120705065+Bhhsaurabh@users.noreply.github.com>
2024-08-22 15:58:50 +0530

    variable explained

---
+++ 01_basics/02_variables.php
@@ -0,0 +1,12 @@
+<!-- variables are an abstract storage location -->
+ <!-- variables are smart in php its automatically decides the types  -->
+
+ <?php
+ $name = "saurabh";
+ echo $name;
+ 
+ $num = 10*5 ;
+ echo "<br>the output is $num"
+
+
+ ?>
