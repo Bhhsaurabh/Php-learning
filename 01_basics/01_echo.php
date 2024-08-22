efaf6114a0c4bd81adfbe1b87135bba20788c990
Saurabh Kumar <120705065+Bhhsaurabh@users.noreply.github.com>
2024-08-22 15:38:21 +0530

    echo explained

---
+++ 01_basics/01_echo.php
@@ -0,0 +1,10 @@
+<!-- echo is use to display output -->
+ <!-- echo is case insensetive -->
+
+ <?php
+ echo "saurabh <br>"; echo "kumar <br>"; // br tag are used for new line
+ //or 
+
+ echo("hello my name is saurabh");
+ 
+ ?>
